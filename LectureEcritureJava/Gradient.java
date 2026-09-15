ffb7c0265b1a88e10a317e1408a585626bb42442
KELVIN HIGOUNET <kelvin.higounet@iut.local>
2026-09-15 12:54:07 +0200

    Ajout de Gradient.java et gradient.ppm

---
+++ LectureEcritureJava/Gradient.java
@@ -0,0 +1,24 @@
+public class Gradient {
+    public static void main(String[] args) {
+        int width = 200;
+        int height = 100;
+        Image img = new Image(width, height);
+
+        // Génération du dégradé de bleu (de 0 à 255 de gauche à droite)
+        for (int y = 0; y < img.getHeight(); y++) {
+            for (int x = 0; x < img.getWidth(); x++) {
+                
+                int bleu = (x * 255) / (img.getWidth() - 1);
+                
+                img.setPixel(x, y, 0, 0, bleu);
+            }
+        }
+
+        try {
+            img.save_txt("gradient.ppm");
+            System.out.println("Dégradé créé avec succès !");
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la création du dégradé : " + e.getMessage());
+        }
+    }
+}
--- LectureEcritureJava/Image.java
+++ LectureEcritureJava/Image.java
@@ -13,17 +13,17 @@ public class Image {
     /**
      * Constructeur : initialise une image vide.
      */
-    public Image(int width, int hauteur) {
+    public Image(int width, int height) {
         this.width = width;
         this.height = height;
-        pixels = new int[hauteur][largeur][3];
+        pixels = new int[height][width][3];
     }
 
     /**
      * Définit la couleur d'un pixel à la position (x, y)
      */
     public void setPixel(int x, int y, int r, int g, int b) {
-        if (x >= 0 && x < largeur && y >= 0 && y < hauteur) {
+        if (x >= 0 && x < width && y >= 0 && y < height) {
             pixels[y][x][0] = r;
             pixels[y][x][1] = g;
             pixels[y][x][2] = b;
@@ -34,6 +34,20 @@ public class Image {
      * Sauvegarde l'image au format texte PPM (P3)
      */
     public void save_txt(String filename) throws IOException {
-        // TODO : écrire le fichier PPM avec FileWriter
-    }
+        try (FileWriter writer = new FileWriter(filename)) {
+            writer.write("P3\n");
+            writer.write(width + " " + height + "\n");
+            writer.write("255\n");
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    int r = pixels[y][x][0];
+                    int g = pixels[y][x][1];
+                    int b = pixels[y][x][2];
+                    writer.write(r + " " + g + " " + b + " ");
+                }
+                writer.write("\n");
+            }
+        }
+    } 
 }
