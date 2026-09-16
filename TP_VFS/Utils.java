642e77ea781d7170cb94725d01efa5194deec508
KELVIN HIGOUNET <kelvin.higounet@iut.local>
2026-09-16 12:01:52 +0200

    Completion de writeInt

---
+++ TP_VFS/Utils.java
@@ -0,0 +1,27 @@
+public class Utils {
+
+    public static int writeInt(byte[] memory, int offset, int value) {
+        // TODO: Écrire les 4 octets de 'value' dans 'memory'
+        // à partir de 'offset', en big-endian.
+        memory[offset] = (byte) ((value >> 24) & 0xFF);
+        memory[offset +1] = (byte) ((value >> 16) & 0xFF);
+        memory[offset +2] = (byte) ((value >> 8) & 0xFF);
+        memory[offset +3] = (byte) (value & 0xFF);
+        return 4;
+    }
+
+    public static int readInt(byte[] memory, int offset) {
+        // TODO: Reconstituer le int sur 4 octets.
+        return 0;
+    }
+
+    public static int writeShort(byte[] memory, int offset, short value) {
+        // TODO: Écrire les 2 octets de 'value'.
+        return 2;
+    }
+
+    public static short readShort(byte[] memory, int offset) {
+        // TODO: Lire le short sur 2 octets.
+        return 0;
+    }
+}
