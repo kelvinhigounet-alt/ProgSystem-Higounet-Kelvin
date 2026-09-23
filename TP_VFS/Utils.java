d9a06256f9c985d14f6194d0635063ecb5a0b971
KELVIN HIGOUNET <kelvin.higounet@iut.local>
2026-09-23 16:10:04 +0200

    Ajout des méthodes readInt writeShort et readShort

--- TP_VFS/Utils.java
+++ TP_VFS/Utils.java
@@ -12,16 +12,22 @@ public class Utils {
 
     public static int readInt(byte[] memory, int offset) {
         // TODO: Reconstituer le int sur 4 octets.
-        return 0;
+        return ((memory[offset] & 0xFF) << 24) |
+               ((memory[offset + 1] & 0xFF) << 16) |
+               ((memory[offset + 2] & 0xFF) << 8)  |
+                (memory[offset + 3] & 0xFF);
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
         // TODO: Écrire les 2 octets de 'value'.
+        memory[offset]= (byte) ((value >> 8) & 0xFF);
+        memory[offset + 1] = (byte) (value & 0xFF);
         return 2;
     }
 
     public static short readShort(byte[] memory, int offset) {
         // TODO: Lire le short sur 2 octets.
-        return 0;
+        return (short) (((memory[offset]     & 0xFF) << 8) |
+                         (memory[offset + 1] & 0xFF));
     }
 }
