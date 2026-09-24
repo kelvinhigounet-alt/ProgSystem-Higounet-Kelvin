7a66854a5595fd5c156b4e0458b73f302bb7fa37
Kelvin H. <kelvin.higounet@iut-rodez.fr>
2026-09-24 17:13:09 +0200

    Implement methods for long and string memory operations
    Added methods to write and read long values, and to write and read strings in memory.

--- TP_VFS/Utils.java
+++ TP_VFS/Utils.java
@@ -25,9 +25,45 @@ public class Utils {
         return 2;
     }
 
+    public static int writeLong(byte[] memory, int offset, long value) {
+    // TODO: Écrire les 8 octets du long en big-endian.
+    return 8;
+    }
+    
+    public static long readLong(byte[] memory, int offset) {
+        // TODO: Reconstituer le long.
+        return 0L;
+    }
+    
+    public static int writeString(
+            byte[] memory,
+            int offset,
+            String str,
+            int maxLength) {
+    
+        // TODO:
+        // 1. Convertir la chaîne en octets.
+        // 2. Copier les octets sans dépasser maxLength.
+        // 3. Nettoyer le reste de la zone avec des zéros.
+    
+        return maxLength;
+    }
+    
+    public static String readString(
+            byte[] memory,
+            int offset,
+            int maxLength) {
+    
+        // TODO:
+        // Lire jusqu'au premier octet nul
+        // ou jusqu'à maxLength.
+    
+        return "";
+    }
+    
     public static short readShort(byte[] memory, int offset) {
         // TODO: Lire le short sur 2 octets.
         return (short) (((memory[offset]     & 0xFF) << 8) |
-                         (memory[offset + 1] & 0xFF));
+                        (memory[offset + 1] & 0xFF));
     }
 }
